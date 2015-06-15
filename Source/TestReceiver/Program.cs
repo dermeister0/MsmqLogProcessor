75de5aa4fff77340e3d6867418d7f0f3bbe177ea
Anton Zimin <anton@saritasa.com>
2015-06-15 20:02:12 +0700

    Add UTF-8 test. Create simple queue reader.

---
+++ Source/TestReceiver/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Messaging;
+
+namespace TestReceiver
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var queue = new MessageQueue(ConfigurationManager.AppSettings["QueueName"]);
+
+            for (var i = 0; i < 10; i++)
+            {
+                try
+                {
+                    var message = queue.Receive(TimeSpan.FromSeconds(5));
+                    if (message == null)
+                        continue;
+
+                    using (var reader = new StreamReader(message.BodyStream))
+                    {
+                        var body = reader.ReadToEnd();
+                        Console.WriteLine(body);
+                    }
+                }
+                catch (MessageQueueException ex)
+                {
+                    if (ex.MessageQueueErrorCode != MessageQueueErrorCode.IOTimeout)
+                        throw;
+                }
+            }
+        }
+    }
+}
--- Source/TestSender/Program.cs
+++ Source/TestSender/Program.cs
@@ -8,7 +8,7 @@ namespace TestSender
         private static void Main(string[] args)
         {
             var ex = new ArgumentException("args are wrong.");
-            LogManager.GetCurrentClassLogger().Error(ex, "Only test.");
+            LogManager.GetCurrentClassLogger().Error(ex, "Only test. Съешь же ещё этих мягких французских булок да выпей чаю.");
         }
     }
 }
