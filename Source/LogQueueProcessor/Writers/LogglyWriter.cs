a75b22e67f48c4fd42be6b7e4dfd8d2792834858
Anton Zimin <anton@saritasa.com>
2015-07-14 19:12:33 +0700

    Fix errors in LogglyWriter.

--- Source/LogQueueProcessor/Writers/LogglyWriter.cs
+++ Source/LogQueueProcessor/Writers/LogglyWriter.cs
@@ -9,11 +9,11 @@ namespace LogQueueProcessor.Writers
         public void Write(LogRow logRow)
         {
             var e = new LogglyEvent();
-            e.Data.Add("eventDate", logRow.Date);
-            e.Data.Add("level", logRow.Level);
-            e.Data.Add("logger", logRow.Logger);
-            e.Data.Add("message", logRow.Message);
-            e.Data.Add("exception", logRow.Exception);
+            e.Data.Add("eventDate", logRow.Date ?? "");
+            e.Data.Add("level", logRow.Level ?? "");
+            e.Data.Add("logger", logRow.Logger ?? "");
+            e.Data.Add("message", logRow.Message ?? "");
+            e.Data.Add("exception", logRow.Exception ?? "");
             logglyClient.Log(e);
         }
     }
